1c1c2004eff4e0c7627bd47256c48a44a5750c73
CHANDAN KUMAR <70132200+MrTONYCHAN@users.noreply.github.com>
2022-08-23 12:17:57 +0530

    Add files via upload

---
+++ calculator/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./index.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h2 align="center" data-text="TONY CALCULATOR"> TONY CALCULATOR </h2>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
